7258bb807e6d27b739ae4cc7ae757ba138fb059d
caro <caro@dhcp-3-100.gde.emn.fr>
2019-06-26 10:30:41 +0200

    prix

--- CACAO2019/src/abstraction/eq6Distributeur2/Prix.java
+++ CACAO2019/src/abstraction/eq6Distributeur2/Prix.java
@@ -6,6 +6,10 @@ import java.util.HashMap;
 
 import abstraction.eq7Romu.produits.Chocolat;
 import abstraction.eq7Romu.produits.Gamme;
+import abstraction.eq7Romu.ventesContratCadre.ContratCadre;
+import abstraction.eq7Romu.ventesContratCadre.IVendeurContratCadre;
+import abstraction.eq7Romu.ventesContratCadre.StockEnVente;
+import abstraction.fourni.IActeur;
 import abstraction.fourni.Indicateur;
 import abstraction.fourni.Monde;
 
@@ -44,11 +48,10 @@ public class Prix {
         Monde.LE_MONDE.ajouterIndicateur(this.prixHG_E_SHP);
         
         this.margeParProduit = new HashMap<Chocolat, Double>();
-        this.margeParProduit.put(Chocolat.HG_E_SHP, 1.25);
+        this.margeParProduit.put(Chocolat.HG_E_SHP, 1.5);
         this.margeParProduit.put(Chocolat.MG_E_SHP, 1.25);
         this.margeParProduit.put(Chocolat.MG_NE_SHP,1.25);
-        this.margeParProduit.put(Chocolat.MG_NE_HP, 1.25);
-
+        this.margeParProduit.put(Chocolat.MG_NE_HP, 1.1);
         
         this.prixachatParProduit =  new HashMap<Chocolat,Double>();
         this.prixachatParProduit.put(Chocolat.HG_E_SHP, this.getPrixHG_E_SHP().getValeur());
@@ -127,7 +130,8 @@ public class Prix {
 	
     public void ajustementMarge(ArrayList<Double> historique, Chocolat c ) {
         // je récupère le chocoalt et l'historique des variations 
-        int n = historique.size();
+        
+    	/*int n = historique.size();
         int t = nous.getIndicateurStock(c).getHistorique().getTaille();
         if (n>3 && t>3) {
         		double stockprecedent = nous.getIndicateurStock(c).getHistorique().get(t-1).getValeur();
@@ -148,8 +152,54 @@ public class Prix {
         		}		
         }
         }
+        */
 
-}
+    	//moyenne de vente
+    	double vente_stockC= 0;
+    	int steps = 0;
+
+    	if (Monde.LE_MONDE.getStep() >= 24) {
+    		for (int k=0 ; k<historique.size()-1; k++) {
+    			vente_stockC+=historique.get(k);
+    			steps+=1;
+    		}
+    	}
+
+    	double moyenneventeavant = vente_stockC/steps;
+
+    	//moyenne prix vendeur pour une quantité moyenne
+    	double moyenneprixvendeur =0;
+    	int nbvendeur =0;
+
+    	for (IActeur acteur : Monde.LE_MONDE.getActeurs()) {
+    		if (acteur instanceof IVendeurContratCadre<?>) {
+    			IVendeurContratCadre<Chocolat> vacteur = (IVendeurContratCadre<Chocolat>)acteur;
+    			StockEnVente<Chocolat> stock = vacteur.getStockEnVente();
+    			if (stock.get(c) >500) {
+    				nbvendeur+=1;
+    				moyenneprixvendeur+=vacteur.getPrix(c, moyenneventeavant);
+    			}
+    		}
+    	}
+    	moyenneprixvendeur/=nbvendeur;
+
+    	//baisse de la marge si les dernières ventes sont mauvaises et si notre prix reste au dessus de 0.95*moyenneprixvendeur
+    	if (historique.size() > 24 && historique.get(historique.size() -1) < moyenneventeavant*0.7) {
+    		if (getPrixParProduit(c) > moyenneprixvendeur) {
+    			double nouvellemarge = this.getMargeParProduit(c)*0.95;
+    			setMargeParProduit(c, nouvellemarge);
+    		}
+    	}
+
+    	//augmentation de la marge si les dernières ventes sont hautes et si prix actuel pas trop grand
+    	if (historique.size() > 24 && historique.get(historique.size() -1) > moyenneventeavant*2) {
+    		if (getPrixParProduit(c) <1.7*moyenneprixvendeur) {
+    			double nouvellemarge = this.getMargeParProduit(c)*1.05;
+    			setMargeParProduit(c, nouvellemarge);
+    		}
+    	}
+    }
+
     public double getPrixParProduit(Chocolat c) {
     	return getIndicateurPrix(c).getValeur();
     }
